c2f0c8fc9b6277becc52c3cef4f2803a6cfc0591
MarionObrist <marion.obrist@students.fhnw.ch>
2022-12-13 08:22:30 +0100

    update Uebung09

--- Uebung09/geodetic.py
+++ Uebung09/geodetic.py
@@ -9,7 +9,7 @@ g = pyproj.Geod(ellps='WGS84')
 
 
 @app.get("/geodetic/&")
-async def geodetic(startlong:float, startlat:float, endlong=float, endlat=float, punkte=int): 
+async def geodetic(startlong:float, startlat:float, endlong:float, endlat:float, punkte:int): 
     lonlats = g.npts(startlong, startlat, endlong, endlat, punkte)
     lonlats = [[startlong, startlat]] + [list(i) for i in lonlats] + [[endlong, endlat]] ## add start and end point
 
